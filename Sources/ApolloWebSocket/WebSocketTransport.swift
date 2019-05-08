8211a625cdfe8789df974b18ab389bc83b28239d
Kevin Delannoy <kevin.delannoy@thescore.com>
2019-05-08 11:11:34 -0400

    Made connectingPayload public

--- Sources/ApolloWebSocket/WebSocketTransport.swift
+++ Sources/ApolloWebSocket/WebSocketTransport.swift
@@ -42,9 +42,11 @@ public class WebSocketTransport {
   private var acked = false
   
   private var queue: [Int: String] = [:]
-  private var connectingPayload: GraphQLMap?
+
+  public var connectingPayload: GraphQLMap?
   
   private var subscribers = [String: (Result<JSONObject, Error>) -> Void]()
+
   private var subscriptions : [String: String] = [:]
   
   private let sendOperationIdentifiers: Bool
