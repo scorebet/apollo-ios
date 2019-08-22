6ff25fe04e40cbe9a8ec8efa67f41eb770e353ec
Kevin Delannoy <kevin.delannoy@thescore.com>
2019-08-22 11:17:23 -0400

    Fixes in-memory cache race condition
    Grabed the code from https://github.com/apollographql/apollo-ios/pull/552

--- Sources/Apollo/InMemoryNormalizedCache.swift
+++ Sources/Apollo/InMemoryNormalizedCache.swift
@@ -1,21 +1,35 @@
 public final class InMemoryNormalizedCache: NormalizedCache {
-  private var records: RecordSet
+    private var records: RecordSet
+    private let queue: DispatchQueue
 
-  public init(records: RecordSet = RecordSet()) {
-    self.records = records
-  }
+    public init(records: RecordSet = RecordSet()) {
+        self.records = records
+        self.queue = DispatchQueue(label: "com.apollographql.InMemoryNormalizedCache", attributes: .concurrent)
+    }
 
-  public func loadRecords(forKeys keys: [CacheKey]) -> Promise<[Record?]> {
-    let records = keys.map { self.records[$0] }
-    return Promise(fulfilled: records)
-  }
+    public func loadRecords(forKeys keys: [CacheKey]) -> Promise<[Record?]> {
+        return Promise<[Record?]> { fulfill, reject in
+            queue.async(flags: .barrier) {
+                let records = keys.map { self.records[$0] }
+                fulfill(records)
+            }
+        }
+    }
 
-  public func merge(records: RecordSet) -> Promise<Set<CacheKey>> {
-    return Promise(fulfilled: self.records.merge(records: records))
-  }
+    public func merge(records: RecordSet) -> Promise<Set<CacheKey>> {
+        return Promise<Set<CacheKey>> { fulfill, reject in
+            queue.async(flags: .barrier) {
+                fulfill(self.records.merge(records: records))
+            }
+        }
+    }
 
-  public func clear() -> Promise<Void> {
-    records.clear()
-    return Promise(fulfilled: ())
-  }
+    public func clear() -> Promise<Void> {
+        return Promise<Void> { fulfill, reject in
+            queue.async {
+                self.records.clear()
+                fulfill(())
+            }
+        }
+    }
 }
