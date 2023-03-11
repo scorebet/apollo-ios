fea3b7df0f229a2cf0ed72301db71a68fb68ec90
Mahmood Tahir <mahmood.tahir@thescore.com>
2023-03-10 20:41:45 -0500

    Add option to remove merge fragment spreads
    # Conflicts:
    #	Sources/ApolloCodegenLib/Templates/SelectionSetTemplate.swift

--- Sources/ApolloCodegenLib/Templates/SelectionSetTemplate.swift
+++ Sources/ApolloCodegenLib/Templates/SelectionSetTemplate.swift
@@ -293,7 +293,7 @@ struct SelectionSetTemplate {
     \(ifLet: selections.direct?.fields.values, {
       "\($0.map { FieldAccessorTemplate($0, in: scope) }, separator: "\n")"
       })
-    \(selections.merged.fields.values.map { FieldAccessorTemplate($0, in: scope) }, separator: "\n")
+    \(if: config.config.options.fragmentMergingStrategy == .mergeAllFragmentSpreads, "\(selections.merged.fields.values.map { FieldAccessorTemplate($0, in: scope) }, separator: "\n")")
     """
   }
 
@@ -326,7 +326,7 @@ struct SelectionSetTemplate {
     \(ifLet: selections.direct?.inlineFragments.values, {
         "\($0.map { InlineFragmentAccessorTemplate($0) }, separator: "\n")"
       })
-    \(selections.merged.inlineFragments.values.map { InlineFragmentAccessorTemplate($0) }, separator: "\n")
+    \(if: config.config.options.fragmentMergingStrategy == .mergeAllFragmentSpreads, "\(selections.merged.inlineFragments.values.map { InlineFragmentAccessorTemplate($0) }, separator: "\n")")
     """
   }
 
@@ -362,9 +362,7 @@ struct SelectionSetTemplate {
       \(ifLet: selections.direct?.fragments.values, {
         "\($0.map { FragmentAccessorTemplate($0, in: scope) }, separator: "\n")"
         })
-      \(selections.merged.fragments.values.map {
-          FragmentAccessorTemplate($0, in: scope)
-        }, separator: "\n")
+      \(if: config.config.options.fragmentMergingStrategy == .mergeAllFragmentSpreads, "\(selections.merged.fragments.values.map { FragmentAccessorTemplate($0, in: scope) }, separator: "\n")")
     }
     """
   }
@@ -532,8 +530,8 @@ struct SelectionSetTemplate {
     \(ifLet: selections.direct?.inlineFragments.values, {
         "\($0.map { render(inlineFragment: $0) }, separator: "\n\n")"
       })
-    \(selections.merged.inlineFragments.values.map { render(inlineFragment: $0) }, separator: "\n\n")
-    """    
+    \(if: config.config.options.fragmentMergingStrategy == .mergeAllFragmentSpreads, "\(selections.merged.inlineFragments.values.map { render(inlineFragment: $0) }, separator: "\n\n")")
+    """
   }
 
   // MARK: - SelectionSet Name Computation
@@ -765,7 +763,7 @@ fileprivate extension IR.ScopeCondition {
     \(ifLet: conditions, { "If\($0.typeNameComponents)"})
     """).description
   }
-  
+
 }
 
 fileprivate extension AnyOf where T == IR.InclusionConditions {
